6a5c6d6ac5da26dd0d781ad8e91fdac0f137f78a
Robert Jackson <rjackson@linkedin.com>
2017-02-22 17:26:43 -0500

    Flesh out CSS minification process.

---
+++ broccoli-clean-css.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const path = require('path');
+
+const CleanCSS = require('clean-css');
+const BroccoliPersistentFilter = require('broccoli-persistent-filter');
+const jsonStableStringify = require('json-stable-stringify');
+
+class CleanCSSFilter extends BroccoliPersistentFilter {
+  constructor(inputTree, options) {
+    super(inputTree, options);
+
+    this.options = options || {};
+  }
+
+  baseDir() {
+    return __dirname;
+  }
+
+  cacheKeyProcessString(string, relativePath) {
+    this.optionsHash = this.optionsHash || jsonStableStringify(this.options);
+
+    return `${this.optionsHash}${super.cacheKeyProcessString(string, relativePath)}`;
+  }
+
+  setupCleanCSS() {
+    let inputPath = this.inputPaths[0];
+    let cleanCSSOptions = Object.assign({}, this.options.cleanCSS, { returnPromise: true });
+
+    if (cleanCSSOptions.rebaseTo) {
+      cleanCSSOptions.rebaseTo = path.join(inputPath, cleanCSSOptions.rebaseTo);
+    }
+
+    this.cleanCSS = new CleanCSS(cleanCSSOptions);
+  }
+
+  build() {
+    if (!this.cleanCSS) { this.setupCleanCSS(); }
+
+    return super.build();
+  }
+
+  processString(contents, relativePath) {
+    let fullPath = path.resolve(this.inputPaths[0], relativePath);
+
+    return this.cleanCSS.minify({
+      [fullPath]: { styles: contents }
+    }).then(result => result.styles);
+  }
+}
+
+CleanCSSFilter.prototype.extensions = ['css'];
+CleanCSSFilter.prototype.targetExtension = 'css';
+
+module.exports = CleanCSSFilter;
--- index.js
+++ index.js
@@ -2,5 +2,27 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-clean-css'
+  name: 'ember-cli-clean-css',
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type !== 'parent') { return; }
+
+    let addon = this;
+
+    registry.add('minify-css', {
+      name: 'ember-cli-clean-css:preprocessor',
+      ext: 'css',
+      toTree(tree) {
+        let minifyOptions = addon._findHost().options.minifyCSS;
+
+        if (!minifyOptions.enabled) { return tree; }
+
+        const CleanCSSPlugin = require('./broccoli-clean-css');
+
+        return new CleanCSSPlugin(tree, {
+          cleanCSS: minifyOptions.options
+        });
+      }
+    })
+  }
 };
